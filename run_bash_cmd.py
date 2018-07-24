6049169420e054585b7aa5eb3289097d16036c6c
BhanuKiranChaluvadi <nthdimensionalreality@gmail.com>
2018-07-24 14:46:44 +0200

    Added file run_bash_cmd.py
    Sample python script that can be used to run the bash commands or any type of the commands that can run on terminal.

---
+++ run_bash_cmd.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import argparse
+import os
+import os.path
+import sys
+from subprocess import call
+
+# cd /home/bc/Desktop/laptics/run_cmd
+# python main.py --input_dir=/home/bc/converted_audio/recordings --output_dir=/home/bc/converted_audio/loudest_section
+
+# --input_dir=/home/bc/converted_audio/recordings
+# --output_dir=/home/bc/converted_audio/loudest_section
+
+os.chdir("/tmp/extract_loudest_section/gen/bin")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input_dir', '-i',
+        type=str,
+        help="""\
+        Where to find the input files.
+        """)
+    parser.add_argument(
+        '--output_dir', '-o',
+        type=str,
+        help="""\
+        Where to save the output files.
+        """)
+
+    args = parser.parse_args()
+
+    for file in os.listdir(args.input_dir):
+        if file.endswith(".wav"):
+            # Function call
+            # call(["ls", "-l"])
+            # call(["pwd"])
+            call(["./extract_loudest_section", os.path.join(args.input_dir, file), args.output_dir])
